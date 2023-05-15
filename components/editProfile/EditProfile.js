7d16f9925c89b36b8afd5522e350fdb67fcff6ba
Valentin Meier <valentinmeier123@gmail.com>
2023-05-15 11:52:35 -0300

    ultimos cambios

--- components/editProfile/EditProfile.js
+++ components/editProfile/EditProfile.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageDragDrop from "../common/imageDrop/ImageDragDrop";
 import Styles from "./editProfile.module.css";
+import { useData } from "@/context/dataContext"; 
+import { useRouter } from "next/router"; 
 
 const EditProfile = () => {
-  let handleImageDrop = (imageFile) => {
-    console.log("Imagen cargada", imageFile);
+  const [image, setImage] = useState(null);
+  const dataUserCurrent = useData(); 
+  const router = useRouter(); 
+  console.log(dataUserCurrent)
+  const handleImageDrop = (imageFile) => {
+    setImage(imageFile);
   };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("profile_image", image);
+
+    try {
+      const response = await fetch("/api/uploadImportantDocument", {
+        
+        method: "POST",
+        headers: {
+          token: dataUserCurrent.token,
+          uid: dataUserCurrent.userData.response.response.userId,
+        },
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.status === 200) {
+        router.reload(); 
+      } else {
+        console.error("Error al subir la imagen:", data.response);
+      }
+    } catch (error) {
+      console.error("Error al subir la imagen:", error);
+    }
+  };
+
   return (
     <section className={Styles.infoContainer}>
       <div className={Styles.profileImage}>
         <ImageDragDrop onImageDrop={handleImageDrop} />
       </div>
+      {image ? <button onClick={handleUpload}>Subir</button> : ""}
     </section>
   );
 };
